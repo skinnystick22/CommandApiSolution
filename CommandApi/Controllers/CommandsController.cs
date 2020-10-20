b92e67d06e743fcbd4831cb12d87fac07e348ad3
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-10-20 10:51:40 -0500

    Initial Project Files

---
+++ CommandApi/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] {"this", "is", "hard", "coded"};
+        }
+    }
+}
