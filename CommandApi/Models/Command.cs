9fd901e1483083bb04b480ca4c02caa09e840a38
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-10-20 18:19:28 -0500

    Added Command Model to API Project

---
+++ CommandApi/Models/Command.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandApi.Models
+{
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int CommandId { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        
+        [Required]
+        public string Platform { get; set; }
+        
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
