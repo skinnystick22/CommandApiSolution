5967fd78f44609ab58550f8b263b0150371a1bf1
Tyler Miller <tyler.e.miller1995@gmail.com>
2020-10-20 20:52:04 -0500

    Added CommandContext and Entity Framework Core Packages

---
+++ CommandApi/Data/CommandContext.cs
@@ -0,0 +1,15 @@
+﻿using CommandApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandApi.Data
+{
+    public class CommandContext : DbContext
+    {
+        public DbSet<Command> CommandItems { get; set; }
+        
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        {
+            
+        }
+    }
+}
